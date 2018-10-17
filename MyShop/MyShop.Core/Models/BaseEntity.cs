45034768ccaa7d4e980997e1c138c5237e4a3b4a
David Amos <amosda4183@gmail.com>
2018-10-17 19:38:40 -0400

    Generic Repoository In Memory and BaseEntity class created.

---
+++ MyShop/MyShop.Core/Models/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public abstract class BaseEntity
+    {
+        public string Id { get; set; }
+        //saves the time when the class implementing the base class was created 
+        public DateTimeOffset CreatedAt { get; set; }
+
+        //constructor to setup Id and time created
+        public BaseEntity()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.CreatedAt = DateTime.Now;
+        }
+    }
+}
