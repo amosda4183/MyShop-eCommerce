e60865d67af809780056eff4951aefa2549764a2
David Amos <amosda4183@gmail.com>
2018-10-17 21:56:08 -0400

    Configured SQL Server Database

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //Default constructor
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }  
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> productCategories { get; set; }
+
+        //SQL setup commands
+        //Enable-Migrations
+        //Add-Migration (generic name) Initial - generarates commands to create tables in the database
+        //Update-Database - creates database tables generated from migration
+        
+    }
+}
